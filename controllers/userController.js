4b9beddbca71e71a2ceedacf0f9a6750dc29f2a3
Deepak Lokare <deepaklokare.24@gmail.com>
2022-11-21 13:23:32 +0530

    reading params from url

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,13 +2,19 @@ exports.signUp = async(req, res, next) => {
     return res.status(200).send({message:"hitting the signup route"})
 }
 
-exports.logIn = async(req, res) => {
-    return res.status(200).send({message:"hitting the login route"})
-}
+exports.logIn = async (req, res) => {
+  res.status(200).send({ message: "hitting the login route" });
+};
 
-exports.updateUser = async(req, res) => {
-   return res.status(200).send({message:"hitting the update user route"})
-}
+exports.updateUser = async (req, res) => {
+  console.log(req.params);
+  return res
+    .status(200)
+    .send({
+      message:
+        "hitting the update user route with user id : " + req.params.userId,
+    });
+};
 
 exports.deleteUser = async(req, res) => {
     console.log("reached here");
